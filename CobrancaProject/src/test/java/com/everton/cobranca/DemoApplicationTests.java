3f7530b52368acd49692b32110ff7a149339c3e4
Everton Godoy <everton.mgodoy@gmail.com>
2020-05-19 00:00:30 -0300

    2.2. Protótipo do cadastro de título
    Full class 2.2. Protótipo do cadastro de título

---
+++ CobrancaProject/src/test/java/com/everton/cobranca/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everton.cobranca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
