17e3038d1c52b93d71f20e2fb0d0c3bfbb3e74b2
Everton Godoy <everton.mgodoy@gmail.com>
2020-05-23 17:40:18 -0300

    2.5. Mostrando a mensagem de sucesso
    Full class 2.5. Mostrando a mensagem de sucesso, and banner.txt

--- CobrancaProject/src/main/java/com/everton/cobranca/controller/TituloController.java
+++ CobrancaProject/src/main/java/com/everton/cobranca/controller/TituloController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.everton.cobranca.TitulosRepo;
 import com.everton.cobranca.model.Titulo;
@@ -22,15 +23,18 @@ public class TituloController {
     }
     
     @RequestMapping(method = RequestMethod.POST)
-    public String salvar(Titulo tituloCob) {
+    public ModelAndView salvar(Titulo tituloCob) {
         System.out.println(" ## Salvou titulo aqui = " + tituloCob.getDescricao());
         System.out.println(" ## Salvou titulo aqui = " + tituloCob.getStatus());
         System.out.println(" ## Salvou titulo aqui = " + tituloCob.getDataVencimento());
         System.out.println(" ## Salvou titulo aqui = " + tituloCob.getValor());
         
+        ModelAndView mv = new ModelAndView("CadastroTitulo");
+        mv.addObject("msg", "Titulo salvo corretamente");
+        
         titulosRepository.save(tituloCob);
         
-        return "CadastroTitulo";
+        return mv;
     }
     
     /*
