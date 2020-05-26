540c8c0ed7a50909ec9ffe055b675620c218b1ae
Everton Godoy <everton.mgodoy@gmail.com>
2020-05-26 01:25:25 -0300

    Solucao para manter a Action original
    Solucao feita por mim para manter a Action original apos fechar o Modal no video 2.16. Implementando a exclusao.
    Havera ainda uma solucao no video 2.17. Corrigindo bug exclusao, mas ainda nao visualizei.

--- CobrancaProject/src/main/resources/static/js/cobranca.js
+++ CobrancaProject/src/main/resources/static/js/cobranca.js
@@ -1,17 +1,22 @@
 $('#modalConfirmacaoExclusao').on('show.bs.modal', function(event){
 	                               // Quando esse evento acontecer, vai executar a funcao
 	
-	var botao = $(event.relatedTarget);
+	var botao = $(event.relatedTarget); // Trata-se do " x ", para Excluir
 	
 	var codigoTitulo = botao.data('codigotitulo');
 	var descricaoTitulo = botao.data('desctitulo');
 	
-	
-	// alert(codigoTitulo);
-	
 	var modal = $(this);
 	var form = modal.find('form');
 	var action = form.attr('action');
+	
+	// Guardar a action original
+	if($('#actionOriginal').val() == ''){
+		$('#actionOriginal').val(action);
+		form.attr('actionOriginal', $('#actionOriginal').val());
+	}
+	//
+	
 	if(!action.endsWith('/')){
 		action += '/';
 	}
@@ -21,3 +26,23 @@ $('#modalConfirmacaoExclusao').on('show.bs.modal', function(event){
 	
 });
 
+
+/**
+ * Everton Godoy
+ * Solucao para resolver o problema do video 
+ * 2.16. Implementando a exclusao
+ * Foi implementada uma solucao no video
+ * 2.17. Corrigindo bug exclusao
+ * Mas ainda nao assisti. Essa corecao foi minha 
+ * 
+ * @param event
+ * @returns
+ */
+$('#modalConfirmacaoExclusao').on('hide.bs.modal', function(event){
+	
+	var modal = $(this);
+	var form = modal.find('form');
+	form.attr('action', $('#actionOriginal').val());
+	$('#actionOriginal').val('');
+	
+});
