f3709c34ede2f0123499853a96970ab27a71b4c9
Everton <everton.mgodoy@gmail.com>
2020-05-27 13:27:26 -0300

    3.3. Máscara de entrada de valores
    Adicao do JS de Mascara para o campo do tipo de valor em Moeda em Cadastro de Titulo

--- CobrancaProject/src/main/resources/static/js/cobranca.js
+++ CobrancaProject/src/main/resources/static/js/cobranca.js
@@ -57,4 +57,46 @@ $(function(){
 	$('.glyphicon-pencil').tooltip({title: "Editar Titulo", placement: "top"});
 	$('.glyphicon-remove').tooltip({title: "Excluir Titulo", placement: "top"});
 	
+	
+	// Tratamento do Currency/Maskmoney
+	// 1. Encontre todos os elementos que tenham a classe eg-moeda -> $('.eg-moeda'), e chame a funcao maskMoney();, podem ser passadas opcoes
+	$('.eg-moeda').maskMoney({decimal: ',' , thousands: '.' , allowZero: true, });
+	
+	
 });
+
+
+/*
+ MaskMoney
+
+***************************************************************
+ Nesse exemplo foi utilizado CSS para fazer a selecao
+***************************************************************
+https://github.com/plentz/jquery-maskmoney ,  baixar em "dist"
+
+Exemplo:
+
+ Para um input:
+
+   <input type="text" id="currency" />
+
+
+ a hora que a pagina for carregada,
+   $(function() ) {
+      . . . 
+   })
+
+ Voce seleciona o elemento
+   . . .
+      $('#currency')
+   . . . 
+
+ e chama a funcao maskMoney
+  . . .
+     $('...').maskMoney();
+ . . . 
+
+ Tem algumas opcoes que podem ser vistas aqui:
+
+- https://github.com/plentz/jquery-maskmoney#options
+*/
